db76167ea2a292ab2b60183ed588007b4d5eacf8
Gutemberg <gutembergsouzadejesus@gmail.com>
2023-01-03 14:18:57 -0300

    mudadndo server.js para index.js

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-var express = require('express')
-var app = express()
+require("express-async-errors");
 
-app.get('/', function (req, res) {
-  res.send('hello world 2')
-})
+const path = require("path");
 
-app.listen(3000)
+const swaggerDocument = require("./src/docs/swagger.json");
+const swaggerUI = require("swagger-ui-express");
+const uploadConfig = require("./src/configs/upload");
+const AppError = require("./src/utils/AppError");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use("/avatar", express.static(uploadConfig.UPLOADS_FOLDER));
+
+const demoExercisePath = path.resolve(__dirname, "..", "exercises", "gif")
+app.use("/exercise/demo", express.static(demoExercisePath));
+
+const thumbExercisesPath = path.resolve(__dirname, "..", "exercises", "thumb")
+app.use("/exercise/thumb", express.static(thumbExercisesPath));
+
+
+const routes = require("./src/routes");
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
+app.use((err, request, response, next) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
